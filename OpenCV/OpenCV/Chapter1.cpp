a26d8e86c62770a0cb3fefb010ba2548bd8b0ea8
Jinhee Han <jinheehanaaa@gmail.com>
2023-01-22 13:01:35 -0600

    Importing Video

--- OpenCV/OpenCV/Chapter1.cpp
+++ OpenCV/OpenCV/Chapter1.cpp
@@ -6,16 +6,36 @@
 using namespace cv;
 using namespace std;
 
-/// <summary>
-///  ////// Importing Images
-/// </summary>
 
 
+/*
+* 
+// Image
 int main() {
 
 	string path = "Resources/test.png";
 	Mat img = imread(path);
 	imshow("Image", img);
 	waitKey(0); // don't close until we press close button
+
+}
+
+*/
+
+// Video
+int main() {
+	string path = "Resources/test_video.mp4";
+	VideoCapture cap(path);
+	Mat img;
+
+	// Read image in video 1-by-1
+	while (true) {
+
+		cap.read(img);
+		imshow("Image", img);
+		waitKey(1); // delay (in ms)
+	}
+
+
 
 }
